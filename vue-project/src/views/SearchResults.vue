3a27ba0ea5aa06c6fbc0bb1ab207a706eedbb413
Pinapple23 <veronika.fayez45@gmail.com>
2023-12-02 04:27:08 +0200

    add sortby feature

--- vue-project/src/views/SearchResults.vue
+++ vue-project/src/views/SearchResults.vue
@@ -10,6 +10,13 @@
       :initialGuests="$route.query.guests"
       :initialRooms="$route.query.rooms"
     />
+        <!-- Sort By Select Input -->
+    <div class="my-4">
+    <label for="sortBy" class="mr-2">Sort By:</label>
+    <select v-model="sortBy" @change="handleSortChange" id="sortBy" class="p-2 bg-gray-100 rounded-md">
+      <option v-for="option in sortByOptions" :key="option.value" :value="option.value">{{ option.label }}</option>
+    </select>
+    </div>
 
     <!-- Display search results based on the query parameters -->
     <div v-if="loading">Loading...</div>
@@ -42,10 +49,11 @@
  import { ref, onMounted } from 'vue';
 import axios from 'axios';
 import SearchForm from '@/components/SearchForm.vue';
-import SiteNavigation from "../components/SiteNavigation.vue";
+import SiteNavigation from "../components/SiteNavigation.vue"
 import { useRoute } from 'vue-router';
 
-
+const sortBy = ref(''); // Holds the selected sort option
+const sortByOptions = ref([]);
 const searchResults = ref([]);
 const loading = ref(false);
 const error = ref(null);
@@ -57,9 +65,42 @@ onMounted(() => {
   // Fetch search results when the component is mounted
   console.log("dest",route.query.destination)
   console.log("id", route.query.destinationId)
+  fetchSortOptions();
   fetchSearchResults();
 });
 
+
+const fetchSortOptions = async () => {
+  try {
+    const req = {
+      method: 'GET',
+      url: 'https://booking-com15.p.rapidapi.com/api/v1/hotels/getSortBy',
+      params: {
+        dest_id: route.query.destinationId,
+        search_type: 'CITY',
+        arrival_date: route.query.checkin,
+        departure_date: route.query.checkout,
+        adults: route.query.guests,
+        room_qty: route.query.rooms,
+      },
+      headers: {
+        'X-RapidAPI-Key': 'e9f543ecb9msh467c6d3dfafa0b1p1445cajsna03fbaad4ef4',
+        'X-RapidAPI-Host': 'booking-com15.p.rapidapi.com'
+      }
+    };
+
+    const response = await axios.request(req);
+    console.log(response)
+    sortByOptions.value = response.data.data.map(option => ({ label: option.title, value: option.id }));
+  } catch (err) {
+    console.error('Error fetching sort options', err);
+  }
+};
+
+const handleSortChange = () => {
+  // Handle the sort change logic here
+  fetchSearchResults();
+};
 const fetchSearchResults = async () => {
   loading.value = true;
 
@@ -75,7 +116,8 @@ const fetchSearchResults = async () => {
         adults: route.query.guests,
         room_qty: route.query.rooms,
         languagecode: 'en-us',
-        currency_code: 'AED'
+        currency_code: 'AED',
+        sort_by: sortBy.value
       },
       headers: {
         'X-RapidAPI-Key': 'e9f543ecb9msh467c6d3dfafa0b1p1445cajsna03fbaad4ef4',
